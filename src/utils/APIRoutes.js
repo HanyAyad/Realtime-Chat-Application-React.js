f9c8ad093632a23f25b209987b99be651ec7754f
Hany Eskandar <52006612+HanyAyad@users.noreply.github.com>
2023-02-20 12:52:47 +0100

    Update APIRoutes.js

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = `${process.env.HOST}`;
+export const host = `${process.env.REAC_APP_HOST}`;
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
